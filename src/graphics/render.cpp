1868314006d5cd89c7c6e4af6e6e0aa36e218554
Lauri Kasanen <curaga@operamail.com>
2013-06-24 17:11:54 +0300

    Better rounding

--- src/graphics/render.cpp
+++ src/graphics/render.cpp
@@ -131,6 +131,7 @@ void IrrDriver::renderGLSL(float dt)
     {
         overridemat.Material.MaterialType = m_shaders->getShader(ES_MIPVIZ);
         overridemat.EnableFlags |= video::EMF_MATERIAL_TYPE;
+        overridemat.EnablePasses = scene::ESNRP_SOLID;
     }
 
 
