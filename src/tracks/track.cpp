0e9943097e6e4241288df66a91af7686c374884d
Lauri Kasanen <curaga@operamail.com>
2013-06-26 17:56:11 +0300

    Keep a list of glowing scene nodes

--- src/tracks/track.cpp
+++ src/tracks/track.cpp
@@ -256,6 +256,8 @@ void Track::cleanup()
         material_manager->popTempMaterial();
     }
 
+    irr_driver->clearGlowingNodes();
+
     if(UserConfigParams::logMemory())
     {
         Log::debug("track",
