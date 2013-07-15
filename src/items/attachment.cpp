a9de898fc15680dcfe9c34144589bb1cb265d60f
Lauri Kasanen <curaga@operamail.com>
2013-07-15 16:59:37 +0300

    Make sure attachments are lit up (anvils, bombs etc)

--- src/items/attachment.cpp
+++ src/items/attachment.cpp
@@ -162,6 +162,8 @@ void Attachment::set(AttachmentType type, float time,
         }
     }
     m_node->setVisible(true);
+
+    irr_driver->applyObjectPassShader(m_node);
 }   // set
 
 // -----------------------------------------------------------------------------
