963e93b681ff6b280d8948af612342353c00079c
Lauri Kasanen <curaga@operamail.com>
2013-08-02 22:14:48 +0300

    Use the track disable here too

--- src/graphics/render.cpp
+++ src/graphics/render.cpp
@@ -255,7 +255,8 @@ void IrrDriver::renderGLSL(float dt)
         } // end glow
 
         // Shadows
-        if (!m_mipviz && !m_wireframe && UserConfigParams::m_shadows)
+        if (!m_mipviz && !m_wireframe && UserConfigParams::m_shadows &&
+            World::getWorld()->getTrack()->hasShadows())
         {
             m_scene_manager->setCurrentRendertime(scene::ESNRP_SOLID);
 
