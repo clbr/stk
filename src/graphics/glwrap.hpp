b8112240ab4fb17eba4410c6b356ea0a39a96fe8
Lauri Kasanen <curaga@operamail.com>
2013-08-19 10:05:12 +0300

    Fix the indent of the previous commit

--- src/graphics/glwrap.hpp
+++ src/graphics/glwrap.hpp
@@ -7,13 +7,15 @@
 #    include <GLES/gl.h>
 #elif defined(WIN32)
 #    define _WINSOCKAPI_
-#    include <windows.h>	// has to be included before gl.h because of WINGDIAPI and APIENTRY definitions
+// has to be included before gl.h because of WINGDIAPI and APIENTRY definitions
+#    include <windows.h>
 #    include <GL/gl.h>
 #else
 #    include <GL/gl.h>
 #endif
 
-#include "../../lib/irrlicht/source/Irrlicht/COpenGLDriver.h"	// already includes glext.h, which defines useful GL constants.
-																// COpenGLDriver has already loaded the extension GL functions we use (e.g glBeginQuery)
+// already includes glext.h, which defines useful GL constants.
+// COpenGLDriver has already loaded the extension GL functions we use (e.g glBeginQuery)
+#include "../../lib/irrlicht/source/Irrlicht/COpenGLDriver.h"
 
 #endif
