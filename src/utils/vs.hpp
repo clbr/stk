d007de693a171f6bd887efc57dc0df20f8c1fff0
Lauri Kasanen <curaga@operamail.com>
2013-09-11 14:44:52 +0300

    Modifications for the VS file according to Funto

--- src/utils/vs.hpp
+++ src/utils/vs.hpp
@@ -1,6 +1,7 @@
 // Visual studio workarounds in one place
 
-#if defined(WIN32) && !defined(__CYGWIN__)  && !defined(__MINGW32__)
+#if defined(_MSC_VER)
+#  include <WinSock2.h>
 #  include <windows.h>
 #  include <math.h>
 
